f59eb1fecaeaeb160bb58d4ae9f0e079d58b1e0c
Dino Hurem <dino.hurem@authoritypartners.com>
2022-06-06 23:42:13 +0200

    Added transactions container

--- lib/screens/home/admin/admin_home.dart
+++ lib/screens/home/admin/admin_home.dart
@@ -206,16 +206,15 @@ class _AdminHomeState extends State<AdminHome> {
         ),
         body: SizedBox(
           height: SizeConfig.safeBlockVertical! * 105,
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: SizeConfig.safeBlockHorizontal! * 3,
-              vertical: SizeConfig.safeBlockVertical! * 2,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: SizeConfig.safeBlockVertical! * 32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: SizeConfig.safeBlockHorizontal! * 3,
+                ),
+                child: SizedBox(
+                  height: SizeConfig.safeBlockVertical! * 34,
                   child: ScrollablePositionedList.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: cards.length,
@@ -262,159 +261,158 @@ class _AdminHomeState extends State<AdminHome> {
                     },
                   ),
                 ),
-                Icon(
-                  Icons.keyboard_arrow_up,
+              ),
+              Icon(
+                Icons.keyboard_arrow_up,
+                color: Colors.white,
+                size: SizeConfig.safeBlockVertical! * 2,
+              ),
+              Text(
+                'Tap on card to flip it',
+                style: TextStyle(
                   color: Colors.white,
-                  size: SizeConfig.safeBlockVertical! * 2,
+                  fontWeight: FontWeight.bold,
+                  fontSize: SizeConfig.safeBlockVertical! * 1.5,
                 ),
-                Text(
-                  'Tap on card to flip it',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: SizeConfig.safeBlockVertical! * 1.5,
-                  ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: SizeConfig.safeBlockHorizontal! * 6,
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: SizeConfig.safeBlockHorizontal! * 3,
-                  ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Available',
+                      style: TextStyle(
+                        fontSize: SizeConfig.safeBlockVertical! * 2,
+                        fontWeight: FontWeight.bold,
+                        color: const Color.fromARGB(255, 7, 86, 139),
+                      ),
+                    ),
+                    Text(
+                      'Limit',
+                      style: TextStyle(
+                        fontSize: SizeConfig.safeBlockVertical! * 2,
+                        fontWeight: FontWeight.bold,
+                        color: const Color.fromARGB(255, 7, 86, 139),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: SizeConfig.safeBlockHorizontal! * 6,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '\$ ${cards[currentCardIndex].available}',
+                      style: TextStyle(
+                        fontSize: SizeConfig.safeBlockVertical! * 3,
+                        fontWeight: FontWeight.bold,
+                        color: const Color.fromARGB(255, 7, 86, 139),
+                      ),
+                    ),
+                    Text(
+                      '\$ ${cards[currentCardIndex].limit}',
+                      style: TextStyle(
+                        fontSize: SizeConfig.safeBlockVertical! * 3,
+                        fontWeight: FontWeight.bold,
+                        color: const Color.fromARGB(255, 7, 86, 139),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: SizeConfig.safeBlockVertical! * 1),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: SizeConfig.safeBlockHorizontal! * 6,
+                ),
+                child: SizedBox(
+                  height: SizeConfig.safeBlockVertical! * 6,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Available',
-                        style: TextStyle(
-                          fontSize: SizeConfig.safeBlockVertical! * 2,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(255, 7, 86, 139),
-                        ),
-                      ),
-                      Text(
-                        'Limit',
-                        style: TextStyle(
-                          fontSize: SizeConfig.safeBlockVertical! * 2,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(255, 7, 86, 139),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: SizeConfig.safeBlockHorizontal! * 3,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '\$ ${cards[currentCardIndex].available}',
-                        style: TextStyle(
-                          fontSize: SizeConfig.safeBlockVertical! * 3,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(255, 7, 86, 139),
-                        ),
-                      ),
-                      Text(
-                        '\$ ${cards[currentCardIndex].limit}',
-                        style: TextStyle(
-                          fontSize: SizeConfig.safeBlockVertical! * 3,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(255, 7, 86, 139),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: SizeConfig.safeBlockVertical! * 1),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: SizeConfig.safeBlockHorizontal! * 3,
-                  ),
-                  child: SizedBox(
-                    height: SizeConfig.safeBlockVertical! * 6,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          width: SizeConfig.safeBlockHorizontal! * 68,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(
-                              SizeConfig.safeBlockHorizontal! * 3,
-                            ),
-                            gradient: const LinearGradient(
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
-                              colors: [
-                                Color.fromARGB(255, 31, 138, 209),
-                                Color(0xffff4479),
-                              ],
-                              tileMode: TileMode.mirror,
-                            ),
+                      Container(
+                        width: SizeConfig.safeBlockHorizontal! * 68,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                            SizeConfig.safeBlockHorizontal! * 3,
                           ),
-                          child: Center(
-                            child: Text(
-                              'Send money',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: SizeConfig.safeBlockVertical! * 2.25,
-                              ),
-                            ),
+                          gradient: const LinearGradient(
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            colors: [
+                              Color.fromARGB(255, 31, 138, 209),
+                              Color(0xffff4479),
+                            ],
+                            tileMode: TileMode.mirror,
                           ),
                         ),
-                        Container(
-                          width: SizeConfig.safeBlockHorizontal! * 12,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            gradient: LinearGradient(
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
-                              colors: [
-                                Color(0xffff4479),
-                                Color.fromARGB(255, 31, 138, 209),
-                              ],
-                              tileMode: TileMode.mirror,
-                            ),
-                          ),
-                          child: Center(
-                            child: Icon(
-                              Icons.add,
+                        child: Center(
+                          child: Text(
+                            'Send money',
+                            style: TextStyle(
                               color: Colors.white,
-                              size: SizeConfig.safeBlockVertical! * 3,
+                              fontWeight: FontWeight.bold,
+                              fontSize: SizeConfig.safeBlockVertical! * 2.25,
                             ),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      Container(
+                        width: SizeConfig.safeBlockHorizontal! * 12,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            colors: [
+                              Color(0xffff4479),
+                              Color.fromARGB(255, 31, 138, 209),
+                            ],
+                            tileMode: TileMode.mirror,
+                          ),
+                        ),
+                        child: Center(
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                            size: SizeConfig.safeBlockVertical! * 3,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: SizeConfig.safeBlockVertical! * 2,
-                    ),
-                    child: ListView.builder(
-                      physics: const BouncingScrollPhysics(),
-                      itemCount:
-                          cards[currentCardIndex].transactions.length * 3,
-                      scrollDirection: Axis.vertical,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            color: Colors.red,
-                            height: SizeConfig.safeBlockVertical! * 3,
-                            width: SizeConfig.safeBlockHorizontal! * 5,
-                          ),
-                        );
-                      },
-                    ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: SizeConfig.safeBlockVertical! * 2,
                   ),
-                )
-              ],
-            ),
+                  child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: cards[currentCardIndex].transactions.length * 3,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.red,
+                          height: SizeConfig.safeBlockVertical! * 3,
+                          width: SizeConfig.safeBlockHorizontal! * 5,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
